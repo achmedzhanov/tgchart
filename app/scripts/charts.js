005dae23f6ccc8ccdb995195188dd42fa4c08ba5
Nail Achmedzhanov <achmedzhanov@gmail.com>
2019-03-12 11:13:01 +0400

    chart scale base on svg transformation; there are problems with line width in MS Edge

--- app/scripts/charts.js
+++ app/scripts/charts.js
@@ -19,6 +19,7 @@
                 parent.el.appendChild(this._el);
             else
                 parent.appendChild(this._el);
+            return this;
         }
     
         get el() {
@@ -52,18 +53,52 @@
             
             const {xMin, xMax, yMin, yMax} = getStats();
 
-            const svgEl = createSVG('svg')
-                .style('width', '100%')
-                .style('height', '100%')
-                .attr('viewBox', xMin * xCoef + ' ' + yMin * yCoef + ' ' + xMax * xCoef + ' ' + yMax * yCoef)
-                //.style('vector-effect', 'non-scaling-stroke')
+            const width = xMax - xMin, height = yMax - yMin;
+            const aspectRatio = 2/1;
                 
-                .attr('zoom', 1); // todo set width, height, viewbox etc
-    
+            let yScale = 1 / (height / width) / aspectRatio;
+
+            
+            const viewBoxPadding = 50;
+            const viewBox = [xMin * xCoef - viewBoxPadding,  yMin * yScale -viewBoxPadding,  
+                xMax * xCoef + viewBoxPadding, yMax * yScale + viewBoxPadding];
+                
+            const svgEl = createSVG('svg')
+            .style('width', '100%')
+            .style('height', '100%')
+            .attr('viewBox', `${viewBox[0]} ${viewBox[1]} ${viewBox[2]} ${viewBox[3]}`)
+            //.style('vector-effect', 'non-scaling-stroke')
+            .attr('zoom', 1); // todo set width, height, viewbox etc
+            
+            // TODO chekc view port when xmin really more  0
+
+            // TODO use clip?
+
+            // TODO need to scle coordinates to minimive viewposrt of svg?
+
+            const initalTransform = `matrix(1, 0, 0, ${yScale}, 0, 0)`;
+
+            const lc = 5;
+            const lineGEl =  createSVG('g')
+            .style('stroke-width', '1px')
+                    .style('vector-effect', 'non-scaling-stroke')
+                    .style('stroke', 'gray')
+                    .style('fill', 'none')
+                    .style('vector-effect', 'non-scaling-stroke')
+                    .attr('transform', initalTransform)
+                    .appendTo(svgEl);
+            for(let i = 0;i<=lc;i++) {
+                    let y = yMin + (yMax - yMin) / lc * i;
+                    createSVG('path')
+                    .attr('d',  'M0 ' + y + ' L' + xMax +' ' + y)
+                    .appendTo(lineGEl);    
+                }    
+
             const columnNames = Object.keys(types);
 
-            // todo invert coordinates
-            // calculate max, min
+            // ==>> TODO !!!! invert coordinates !!!!
+
+            const lineElements = {};
 
             for (let columnIndex = 0; columnIndex< columnNames.length; columnIndex++) {
                 const columnName = columnNames[columnIndex], 
@@ -80,21 +115,50 @@
                         d += ' ';
                         d += c[pIdx] * yCoef;
                     }
-                    createSVG('path')
+                    const p =createSVG('path')
                         .attr('d', d)
                         .style('stroke', color)
-                        .style('stroke-width', '3px')
+                        .style('stroke-width', '2px')
                         // vector-effect: non-scaling-stroke
                         .style('vector-effect', 'non-scaling-stroke')
-                        .attr('transform', 'matrix(5, 0, 0, 5, 0, 0)')
+                        .attr('transform', initalTransform)
                         .style('fill', 'none')
                         .appendTo(svgEl);
+                    lineElements[columnName] = p.el; 
                 } else if (t === 'x') {
                     // todo create x axis
                 }
             }
-    
+
             svgEl.appendTo(el);
+
+            //const sliderEl = document.createElement('div');
+            //sliderEl.classList.add('chart-slider');
+
+            const sliderEl = document.createElement('input');
+            sliderEl.setAttribute('type', 'range');
+            sliderEl.setAttribute('min', '0');
+            sliderEl.setAttribute('max', '100');
+            sliderEl.setAttribute('step', '0.1');
+            sliderEl.setAttribute('value', '100');
+            sliderEl.style['width'] = '100%';
+            const onSliderChange = (e) => {
+                const val = Math.max(e.target.value, 3);
+                console.log('range-val', val, performance.now());
+                const t = `matrix(${100/val},0,0,${yScale},0,0)`;
+                for(let l of Object.values(lineElements)) {
+                    l.setAttribute('transform', t);
+                }
+            };
+            sliderEl.onchange = onSliderChange;
+            sliderEl.oninput = onSliderChange;
+
+            el.appendChild(sliderEl);
+
+            /* <input id="vol-control" type="range" min="0" max="100" step="1" oninput="SetVolume(this.value)" onchange="SetVolume(this.value)"></input>  */            
+    
+
+
         }
     
         init();
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,12 +3,13 @@ console.log('Start ....');
 function start() {
     onDocumentReady(() => {
         let data = globalChartsData;
-        createChart({
-            el: document.getElementById('chart0'),
-            chartData: data[0]
-        });
+        for(let i = 0; i<2; i++) {
+            createChart({
+                el: document.getElementById('chart' + i),
+                chartData: data[i]
+            });
+        }
     });
-    
 }
 
 
