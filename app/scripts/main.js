79965743a638546a52fe7846af5ea96aa2db7fc2
Nail Achmedzhanov <achmedzhanov@gmail.com>
2019-03-24 02:19:18 +0400

    fixed browser zoom

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,11 +16,12 @@ onDocumentReady(() => {
         for(let i = 0; i<data.length; i++) {
             const el = document.getElementById('chart' + i);
             const w = el.offsetWidth || 500;
+            const isLandscape = window.innerHeight > window.innerWidth;
             window.createChart({
                 el: el,
                 chartData: data[i],
                 title: 'Chart #' + i,
-                //sizes: {width: w, height: w}
+                sizes: {width: w, height: isLandscape ?  w : w / 2}
             });
         }
         window.createModeSwitcher();
