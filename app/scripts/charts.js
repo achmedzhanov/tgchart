ded7ec005b2f8505b0c49686128a81ca1c5e02bf
Nail Achmedzhanov <achmedzhanov@gmail.com>
2019-04-03 17:44:30 +0400

    pretty check svg

--- app/scripts/charts.js
+++ app/scripts/charts.js
@@ -1334,11 +1334,9 @@
     }
 
 
-    const checkmark = '<svg class="checkmark"  version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="14px" height="14px" viewBox="0 0 45.701 45.7"	>'
+    const checkmark = '<svg class="checkmark"  version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="14px" height="14px" viewBox="0 0 16 16"	>'
     + '<g><g>'
-    + '<path d="M20.687,38.332c-2.072,2.072-5.434,2.072-7.505,0L1.554,26.704c-2.072-2.071-2.072-5.433,0-7.504'
-    + 'c2.071-2.072,5.433-2.072,7.505,0l6.928,6.927c0.523,0.522,1.372,0.522,1.896,0L36.642,7.368c2.071-2.072,5.433-2.072,7.505,0'
-    + 'c0.995,0.995,1.554,2.345,1.554,3.752c0,1.407-0.559,2.757-1.554,3.752L20.687,38.332z"/>'
+    + '<path style="fill:none;fill-opacity:1;fill-rule:evenodd;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1" d="m 1.0539604,8.009901 3.9009901,5 L 15,3.1896045"/>'
     + '</g></g></svg>';
 
     class ToggleGroup {
